c85aa5d3c74f80b3e8495f050b23e8f7873caa18
jenkins <842238676@qq.com>
2019-11-12 18:33:18 +0800

    修改audio的路径

--- settings/DIR_PATH.py
+++ settings/DIR_PATH.py
@@ -2,7 +2,7 @@
 import os
 
 DIR_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PC_PATH = "C:\\Users\\pengfy\\PycharmProjects\\smart-audio\\"
+PC_PATH = "D:\\"
 web_path = DIR_PATH+'\src\py3_web.py'
 result_path = DIR_PATH+'\\settings\\result.txt'
 currtxt_path = DIR_PATH+'\\settings\\current_file.txt'
